131139ebb613e8af4d6a9f27072f4ad6823fbc4d
Prayash Mohapatra <catchprayash@gmail.com>
2015-09-07 03:32:53 +0530

    Added database config

---
+++ connect.php
@@ -0,0 +1,7 @@
+<?php
+	$username = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+	$password = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+	$host = getenv('OPENSHIFT_MYSQL_DB_HOST');
+	$conn = new PDO('mysql:host=$host;dbname=results', $username, $password);
+	if($conn) echo "Yup! Connected to DB!";
+?>
