16ec922a2b9ee7f5bf5192d4a23fc0077f8455e9
Prayash Mohapatra <catchprayash@gmail.com>
2015-09-07 03:40:58 +0530

    DB port added

--- connect.php
+++ connect.php
@@ -2,8 +2,9 @@
 	$username = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
 	$password = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
 	$host = getenv('OPENSHIFT_MYSQL_DB_HOST');
+	$port = getenv('OPENSHIFT_MYSQL_DB_PORT');
 	try {
-		$conn = new PDO('mysql:host=$host;dbname=results', $username, $password);
+		$conn = new PDO('mysql:host=$host:$port;dbname=results', $username, $password);
 		echo "Connected to Database!";
 	}
 	catch(PDOException $e)	{
