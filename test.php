79d85c42970dbb1b93633000cf6fa61043178222
Prayash Mohapatra <catchprayash@gmail.com>
2015-09-07 04:06:31 +0530

    Excecuting insert in PDO

--- test.php
+++ test.php
@@ -3,9 +3,11 @@ try {
 
 	require_once 'db-conf';
 	$conn = newPDO();
-	$stmt = $conn->prepare("SELECT * FROM cgpa_cet");
+	$stmt = $conn->prepare("INSERT INTO cgpa_cet VALUES(null, 1301106071, 'Prayash M', 'Information Tech', 8.3)");
 	$stmt->excute();
 
+	 while($row = $stmt->fetch()) {
+                        print_r($row);
 } catch(PDOException $e) {
 	echo 'ERROR: '.$e->getMessage();
 }
