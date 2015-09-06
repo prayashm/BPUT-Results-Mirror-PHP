0e5d8266b1b60e7c809977960c02ba00e1c92a2b
Prayash Mohapatra <catchprayash@gmail.com>
2015-09-07 04:08:49 +0530

    Checking select stmt

--- test.php
+++ test.php
@@ -3,7 +3,7 @@ try {
 
 	require_once 'db-conf';
 	$conn = newPDO();
-	$stmt = $conn->prepare("INSERT INTO cgpa_cet VALUES(null, 1301106071, 'Prayash M', 'Information Tech', 8.3)");
+	$stmt = $conn->prepare("SELECT * FROM cgpa_cet)");
 	$stmt->excute();
 
 	 while($row = $stmt->fetch()) {
